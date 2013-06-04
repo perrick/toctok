7e75bb0ec2ea159e6566d02b044cb574f78cf128
Perrick Penet-Avez <perrick@noparking.net>
2013-06-04 14:22:08 +0200

    remise en route d'un sample de test unitaire

---
+++ tests/unit/sample.test.php
@@ -0,0 +1,17 @@
+<?php
+/*
+	toctok
+	$Author: perrick $
+	$URL: svn://svn.noparking.net/var/repos/projets/opentime.fr/applications/ofr/inc/bot.inc.php $
+	$Revision: 830 $
+
+	Copyright (C) No Parking 2013 - 2013
+*/
+
+require dirname(__FILE__)."/../../../simpletest/autorun.php";
+require dirname(__FILE__)."/../../inc/require.inc.php";
+
+class Tests_Toctok_Sample extends UnitTestCase {
+	function test_nothing() {
+	}
+}
